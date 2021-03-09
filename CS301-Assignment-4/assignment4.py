e97c52dd63cdbfcaab942031f34d54774968206f
Jj <jminneyiii@gmail.com>
2021-03-09 08:23:16 -0700

    Init Assignment 4

---
+++ CS301-Assignment-4/assignment4.py
@@ -0,0 +1,15 @@
+
+# Assignment 4
+# 3/9/2021
+
+def search_sorted_list(sorted_list, item):
+	pass
+
+def _rec_search(li, low, high, item):
+	pass
+
+def main():
+	li = []
+
+if __name__ == '__main__':
+	main()
